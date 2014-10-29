8b8ee50d0d4a08f536de2f21c68174b885c53cd2
Mateusz Armatys <mateusz.armatys@gmail.com>
2014-10-29 16:11:13 +0100

    Java code generator

--- src/java.lua
+++ src/java.lua
@@ -1,5 +1,32 @@
+local javatree = require("javatree")
+local JavaNode = javatree["JavaNode"]
+local table = require("table")
+
+local function tinsert (t, el)
+  table["insert"](t,#(t) + 1,el)
+end
+local function processTTable (context, node, ttable, topLevel)
+  node:import("pl.makenika.splot.LuaTable")
+  local lineBuf = {}
+  tinsert(lineBuf,"")
+end
+local function findReturnNode (ast)
+  for i, v in ipairs(ast) do
+    if v["tag"] == "Return" then
+      return i, v
+    end
+  end
+  return nil
+end
 local function process (luaModuleName, javaModuleName, plainAst, typecheckedAst)
-  return ""
+  local ctx = {["luaModuleName"] = luaModuleName, ["javaModuleName"] = javaModuleName, ["plainAst"] = plainAst, ["typecheckedAst"] = typecheckedAst, ["tree"] = JavaNode:new()}
+  local i, retNode = findReturnNode(typecheckedAst)
+  if not (i) then
+    error("Could not find the return node.",2)
+  end
+  local rootNode = JavaNode:new()
+  processTTable(ctx,rootNode,retNode,true)
+  return ctx["tree"]:code()
 end
 return {["name"] = "Java", ["process"] = process}
 
