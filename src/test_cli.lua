bb92af7ed5661ad699cabcadda27a8463a4b4375
Mateusz Armatys <mateusz.armatys@gmail.com>
2015-01-23 16:44:12 +0100

    Remove some type errors; update tests; use 'serpent' for table pretty-printing

--- src/test_cli.lua
+++ src/test_cli.lua
@@ -8,30 +8,36 @@ local function getTmpFile(filename)
   return tmpDirPath .. filename
 end
 
+local function testFixture(fixtureName, outFileName)
+  local ok, errMsg = cli.parseArguments({'fixtures/' .. fixtureName .. '.tl', fixtureName, getTmpFile(outFileName)})
+  assert.is_true(ok)
+  if not ok then
+    print(errMsg)
+  end
+end
+
 describe('Test CLI', function()
   it('Should fail because an input file does not exist', function()
     assert.has_error(function()
-      cli.parseArguments({'fixtures/non_existent_file.tl', 'fixture0', getTmpFile('Fixture0.java')})
+      testFixture('non_existent_file', 'Fixture0.java')
     end)
   end)
 
   it('Should not generate fixture1 (because it has a type error)', function()
-    local ok, errMsg = cli.parseArguments({'fixtures/fixture1.tl', 'fixture1', getTmpFile('Fixture1.java')})
-    assert.is.not_true(ok)
+    assert.has_error(function()
+      testFixture('fixture1', 'Fixture1.java')
+    end)
   end)
 
   it('Generates fixture2', function()
-    local ok, errMsg = cli.parseArguments({'fixtures/fixture2.tl', 'fixture2', getTmpFile('Fixture2.java')})
-    assert.is_true(ok)
+    testFixture('fixture2', 'Fixture2.java')
   end)
 
   it('Generates arrays', function()
-    local ok, errMsg = cli.parseArguments({'fixtures/arrays.tl', 'arrays', getTmpFile('Arrays.java')})
-    assert.is_true(ok)
+    testFixture('arrays', 'Arrays.java')
   end)
 
   it('Generates interfaces', function()
-    local ok, errMsg = cli.parseArguments({'fixtures/interfaces.tl', 'interfaces', getTmpFile('Interfaces.java')})
-    assert.is_true(ok)
+    testFixture('interfaces', 'Interfaces.java')
   end)
 end)
